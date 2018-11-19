922c33e4d2fa1295a6a084bf30223a10db182268
欧阳志鹏 <774148572@qq.com>
2018-11-19 17:00:51 +0800

    把本地工程添加到git

---
+++ git-first/src/com/ouyang/git/FirstClass.java
@@ -0,0 +1,18 @@
+package com.ouyang.git;
+
+/**
+ * @author OYZP
+ * @create 2018-11-19
+ **/
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+    }
+}
